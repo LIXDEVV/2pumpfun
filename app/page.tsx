408b0c524088b0c1756849949718c3376924dd83
Lixdevv <jorgeleython2@gmail.com>
2025-11-14 22:04:41 -0500

    Actualizació

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,14 @@ export default function Home() {
 
       {/* Eyes to the Issue Section */}
       <SmartTweetSection 
-        title="Traders are more necessary than developers.
-We manage it; we don't need three times as many developers."
+        title="detrader creator of $TOM, started a revolution, poorly formulated, this could be better."
         tweetUrl="https://x.com/detraderSol/status/1989441686884524049"
         position="right"
       />
 
       {/* Trenches Cannot Survive Section */}
       <SmartTweetSection 
-        title="the trenches cannot survive without the people who run it. traders need it to be worth it to stay."
+        title="Traders are more necessary than developers. We manage it; we don't need three times as many developers."
         tweetUrl="https://x.com/RipJalens/status/1989014926686290217"
         position="left"
       />
